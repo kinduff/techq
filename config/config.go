b2ed319119978cc15b7973bae25de36daaac9515
kinduff <kinduff@protonmail.com>
2022-04-14 12:06:06 +0200

    Moves flags, dotenv, to config

--- config/config.go
+++ config/config.go
@@ -1,13 +1,58 @@
 package config
 
-import "os"
+import (
+	"flag"
+	"os"
 
-type Config struct {
+	"github.com/joho/godotenv"
+	"github.com/kinduff/tech_qa/db"
+	log "github.com/sirupsen/logrus"
+)
+
+var Conf *config
+
+type config struct {
 	Port string
 }
 
-func New() *Config {
-	return &Config{
+func LoadConfig() {
+	loadDotEnv()
+	Conf = new()
+}
+
+func HandleArgs() {
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) >= 1 {
+		switch args[0] {
+		case "seed":
+			db.ExecuteSeed(db.DB, args[1:]...)
+			os.Exit(0)
+		case "create":
+			db.CreateDB(db.DB)
+			os.Exit(0)
+		case "drop":
+			db.DropDB(db.DB)
+			os.Exit(0)
+		case "setup":
+			db.SetupDB(db.DB)
+			db.ExecuteSeed(db.DB)
+			os.Exit(0)
+		}
+	}
+}
+
+func loadDotEnv() {
+	if err := godotenv.Load(); err != nil {
+		log.WithField("event", "dotenv").Info("No .env file found")
+	} else {
+		log.WithField("event", "dotenv").Info("Loaded .env file")
+	}
+}
+
+func new() *config {
+	return &config{
 		Port: getEnv("PORT", "3000"),
 	}
 }
--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"flag"
 	"os"
 	"os/signal"
 	"syscall"
 
-	"github.com/joho/godotenv"
 	log "github.com/sirupsen/logrus"
 
 	"github.com/kinduff/tech_qa/config"
@@ -16,17 +14,13 @@ import (
 
 var (
 	s *server.Server
-
-	// Conf is the global configuration.
-	Conf *config.Config
 )
 
 func main() {
-	loadDotEnv()
-	loadConfig()
+	config.LoadConfig()
 	db.ConnectDatabase()
-	handleArgs()
-	initHTTPServer(Conf.Port)
+	config.HandleArgs()
+	initHTTPServer(config.Conf.Port)
 	handleExitSignal()
 }
 
@@ -43,39 +37,4 @@ func handleExitSignal() {
 
 	s.Stop()
 	log.WithField("event", "server").Fatal("HTTP server stopped")
-}
-
-func handleArgs() {
-	flag.Parse()
-	args := flag.Args()
-
-	if len(args) >= 1 {
-		switch args[0] {
-		case "seed":
-			db.ExecuteSeed(db.DB, args[1:]...)
-			os.Exit(0)
-		case "create":
-			db.CreateDB(db.DB)
-			os.Exit(0)
-		case "drop":
-			db.DropDB(db.DB)
-			os.Exit(0)
-		case "setup":
-			db.SetupDB(db.DB)
-			db.ExecuteSeed(db.DB)
-			os.Exit(0)
-		}
-	}
-}
-
-func loadDotEnv() {
-	if err := godotenv.Load(); err != nil {
-		log.WithField("event", "dotenv").Info("No .env file found")
-	} else {
-		log.WithField("event", "dotenv").Info("Loaded .env file")
-	}
-}
-
-func loadConfig() {
-	Conf = config.New()
 }
